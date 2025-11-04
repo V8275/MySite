d1319dc4ec02e8ea681a004a4adedecdd6c5ab4d
V8275 <87564199+V8275@users.noreply.github.com>
2025-11-04 13:20:43 +0300

    fixed some visualization

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,8 +1,16 @@
+let language = sessionStorage.getItem('selectedLanguage') || 'en';
+
+function switchFirstLang(){
+    switchLanguage(language);
+}
+
 function switchLanguage(lang) {
     const elements = document.querySelectorAll('[data-lang]');
     elements.forEach(element => {
         element.textContent = element.getAttribute(`data-lang-${lang}`);
     });
+    language = lang;
+    sessionStorage.setItem('selectedLanguage', lang);
 }
 
 function redirect(element) {
